fb708491e7d8a79c0ad55cd4853e66e3631f65df
Denis Couras <dcouras@stefanini.com>
2022-08-16 21:42:13 -0300

    teste

--- appFormat/ViewController.swift
+++ appFormat/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         let lbl = UILabel()
         lbl.textColor = Asset.Colors.corBlue.color
     }
